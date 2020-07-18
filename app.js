bcf4abe8c66ab0b31582555e2b45a2457e6eb47b
sungnga <nga422@gmail.com>
2020-07-17 23:59:37 -0700

    Connecting to mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+// app
+const app = express();
+
+// db
+mongoose
+	.connect(process.env.DATABASE, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false
+	})
+	.then(() => console.log('DB Connected'));
+
+// routes
 app.get('/', (req, res) => {
 	res.send('hello from node');
 });
